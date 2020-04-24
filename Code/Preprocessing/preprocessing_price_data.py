4061461c6c8167e1c135e4f6b4df298037a0957c
MeirJoffe <39678648+MeirJoffe@users.noreply.github.com>
2020-04-24 17:41:23 +0300

    fixing all

---
+++ Code/Preprocessing/preprocessing_price_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import os
+import re
+from Code.constants import *
+
+
+
+
+price_paid_headers = ['tid', 'price', 'date', 'postcode', 'property_type', 'old_new', 'duration', 'paon', 'saon',
+                      'street', 'locality', 'city', 'district', 'county', 'ppd_type', 'status']
+
+
+def combine_price_parts(file_1, file_2):
+    df_1 = pd.read_csv(os.path.join(ORIGINAL_PRICE_DATA_PATH, file_1), index_col='Unnamed: 0')
+    df_2 = pd.read_csv(os.path.join(ORIGINAL_PRICE_DATA_PATH, file_2), index_col='Unnamed: 0')
+    new_df = pd.concat([df_1, df_2], ignore_index=True)
+    new_df.index.rename('id', inplace=True)
+    new_df.to_csv(os.path.join(PRICE_DATA_PATH, file_1[:-10] + '.csv'))
+
+
+def add_headers(file_name, path, headers):
+    df = pd.read_csv(os.path.join(path, file_name), header=None)
+    df.columns = headers
+    df.to_csv(os.path.join(path, file_name))
+
+
+def fix_index_col(file_name):
+    df = pd.read_csv(os.path.join(PRICE_DATA_PATH, file_name), index_col='Unnamed: 0')
+    df.index.rename('id', inplace=True)
+    df.to_csv(os.path.join(PRICE_DATA_PATH, file_name))
+
+
+# Add headers to each of the pricing data files.
+for i in range(2013, 2019):
+    add_headers('pp-{}.csv'.format(i), PRICE_DATA_PATH, price_paid_headers)
+for j in range(1999, 2013):
+    add_headers('pp-{}-part1.csv'.format(j), ORIGINAL_PRICE_DATA_PATH, price_paid_headers)
+    add_headers('pp-{}-part2.csv'.format(j), ORIGINAL_PRICE_DATA_PATH, price_paid_headers)
+
+# Combine files that each represent only half of the year's data
+for k in range(1999, 2013):
+    combine_price_parts('pp-{}-part1.csv'.format(k), 'pp-{}-part2.csv'.format(k))
+
+# Set the index column
+for l in range(2013, 2019):
+    fix_index_col('pp-{}.csv'.format(l))
---
+++ Code/constants.py
@@ -0,0 +1,31 @@
+DATA_PATH = 'C:\\Users\\Meir\\PycharmProjects\\Final_DS_Project\\Data'
+PRICE_DATA_PATH = DATA_PATH + '\\Price_Paid'
+ORIGINAL_PRICE_DATA_PATH = DATA_PATH + '\\Price_Paid\\Original_Parts'
+INCOME_DATA_PATH = DATA_PATH + '\\Income_By_District'
+MEAN_INCOME_DATA_PATH = DATA_PATH + '\\Income_By_District\\Mean'
+MEDIAN_INCOME_DATA_PATH = DATA_PATH + '\\Income_By_District\\Median'
+
+district_changes = {'shepway': 'folkestone and hythe', 'bournemouth': 'bournemouth, christchurch and poole',
+                    'christchurch': 'bournemouth, christchurch and poole', 'castle morpeth': 'northumberland',
+                    'east dorset': 'dorset', 'forest heath': 'west suffolk', 'weymouth and portland': 'dorset',
+                    'poole': 'bournemouth, christchurch and poole', 'purbeck': 'dorset', 'carrick': 'cornwall',
+                    'st edmundsbury': 'west suffolk', 'suffolk coastal': 'east suffolk', 'bridgnorth': 'shropshire',
+                    'taunton deane': 'somerset west and taunton', 'waveney': 'east suffolk', 'west dorset': 'dorset',
+                    'west somerset': 'somerset west and taunton', 'south bedfordshire': 'central bedfordshire',
+                    'alnwick': 'northumberland', 'berwick-upon-tweed': 'northumberland', 'west wiltshire': 'wiltshire',
+                    'blyth valley': 'northumberland', 'caradon': 'cornwall', 'chester': 'cheshire west and chester',
+                    'chester-le-street': 'county durham', 'congleton': 'cheshire east', 'derwentside': 'county durham',
+                    'crewe and nantwich': 'cheshire east', 'durham': 'county durham', 'easington': 'county durham',
+                    'ellesmere port and neston': 'cheshire west and chester', 'north shropshire': 'shropshire',
+                    'north dorset': 'dorset', 'kerrier': 'cornwall', 'mid bedfordshire': 'central bedfordshire',
+                    'macclesfield': 'cheshire east', 'north cornwall': 'cornwall', 'north wiltshire': 'wiltshire',
+                    'oswestry': 'shropshire', 'penwith': 'cornwall', 'restormel': 'cornwall', 'salisbury': 'wiltshire',
+                    'shrewsbury and atcham': 'shropshire', 'teesdale': 'county durham', 'tynedale': 'northumberland',
+                    'vale royal': 'cheshire west and chester', 'wear valley': 'county durham', 'kennet': 'wiltshire',
+                    'south shropshire': 'shropshire', 'sedgefield': 'county durham', 'wansbeck': 'northumberland',
+                    'kingston upon hull': 'city of kingston upon hull', 'peterborough': 'city of peterborough',
+                    'derby': 'city of derby', 'westminster': 'city of westminster', 'nottingham': 'city of nottingham',
+                    'bristol, city of': 'city of bristol', 'bristol': 'city of bristol', 'plymouth': 'city of plymouth',
+                    'wrekin': 'telford and wrekin', 'north bedfordshire': 'bedford',
+                    'east yorkshire': 'east riding of yorkshire'}
+
